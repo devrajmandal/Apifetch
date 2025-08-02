19fe0a6ab3b030538c1e8fbcd89edec9390357b5
Devraj <devrajmandal5650@gmail.com>
2025-08-02 12:22:19 +0530

    Add search functionality

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -5,6 +5,7 @@ export const Pokemon = () => {
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
   const API = "https://pokeapi.co/api/v2/pokemon?limit=24";
   const fetchPokemon = async () => {
     try {
@@ -34,6 +35,10 @@ export const Pokemon = () => {
     fetchPokemon();
   }, []);
 
+  const searchData = pokemon.filter((curPokemon) =>
+    curPokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   if (loading) {
     return (
       <div>
@@ -56,9 +61,18 @@ export const Pokemon = () => {
         <header>
           <h1>Let's Catch Pokemon</h1>
         </header>
+        <div className="pokemon-search">
+          <input
+            type="text"
+            placeholder="Search Pokemon"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
         <div>
           <ul className="cards">
-            {pokemon.map((curPokemon) => {
+            {/* {pokemon.map((curPokemon) => { */}
+            {searchData.map((curPokemon) => {
               return (
                 <PokemonCards key={curPokemon.id} pokemonData={curPokemon} />
               );
